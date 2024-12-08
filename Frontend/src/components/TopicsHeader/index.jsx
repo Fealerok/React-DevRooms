02c5b84128c6c9697ca15ba5276b8d3bb7066c89
inemts <ilka311205@gmail.com>
2024-12-08 04:03:30 +0300

    123

---
+++ Frontend/src/components/TopicsHeader/index.jsx
@@ -0,0 +1,14 @@
+import styles from "./index.module.scss";
+
+
+function TopicsHeader(){
+    return(
+        <header className={styles.topicsHeader}>
+            <div>
+                <h3>Раздел 2:</h3>  
+            </div>
+        </header>
+    ) 
+} 
+
+export default TopicsHeader;
